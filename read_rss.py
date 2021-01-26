abb1761a1c8ec13a1f898589789d96de8415ae9c
oipocorp <procopio@gmail.com>
2021-01-25 22:11:50 -0300

    ajustes em nomes de vairaveis e coloquei o item original dentro dos itens salvos

--- read_rss.py
+++ read_rss.py
@@ -1,17 +1,15 @@
 import feedparser
 import time
-from pprint import pprint
-from operator import itemgetter
 import pickle
-from time import mktime
-from datetime import datetime
 import sys
 import blessed
 
+
 term = blessed.Terminal()
 
+
 def get_item_hash(item):
-    return ''.join(list(item.values()))
+    return ''.join([item['date'], item['label'], item['title'], item['link']])
 
 
 def time_to_str(published_time):
@@ -19,64 +17,73 @@ def time_to_str(published_time):
 
 
 def format_news_item(item):
-    return '{}: {}:\n\t{}\n\t{}\n\n'.format(item['date'], item['label'], item['title'], item['link'])
+    return '{}: {}:\n\t{}\n\t{}\n\n'.format(item['date'],
+                                            item['label'],
+                                            item['title'],
+                                            item['link'])
 
 
 def print_item(item):
+    global term
     print(f"{term.yellow(item['date'])}: {term.yellow(item['label'].upper())}:")
     print(f"\t{term.link(item['link'], item['title'])}\n")
 
 
-def load_hash_list():
+def load_saved_items():
     try:
-        with open('hash_list.pickle', 'rb') as pickle_file:
+        with open('saved_items.pickle', 'rb') as pickle_file:
             return pickle.load(pickle_file)
     except FileNotFoundError:
-        return []
+        return {}
 
 
-def save_hash_list(hash_list):
-    with open('hash_list.pickle', 'wb') as pickle_file:
-        pickle.dump(hash_list, pickle_file)
+def dump_saved_items(saved_items):
+    with open('saved_items.pickle', 'wb') as pickle_file:
+        pickle.dump(saved_items, pickle_file)
 
 
 def get_news():
-    hash_list = load_hash_list()
+    saved_items = load_saved_items()
     agregado = []
     with open('files/feeds.txt', 'r') as feed_list_file:
         for config in feed_list_file:
             if config.strip():
                 label, feed = config.strip('\n').split(';')
-                
+
                 try:
-                    NewsFeed = feedparser.parse(feed)
-                except ConnectionResetError:
+                    news_feed = feedparser.parse(feed)
+                except (ConnectionResetError, TimeoutError):
                     print(f"Falha na conexao com {label}")
                     continue
 
-                for entry in NewsFeed.entries:
-                    if entry.published_parsed:
-                        item = {'date': time_to_str(entry.published_parsed),
+                for news_item in news_feed.entries:
+                    if news_item.published_parsed:
+                        item = {'date': time_to_str(news_item.published_parsed),
                                 'label': label,
-                                'title': entry['title'],
-                                'link': entry['link']}
+                                'title': news_item['title'],
+                                'link': news_item['link'],
+                                'news_item': news_item}
                         item_hash = get_item_hash(item)
-                        if item_hash not in hash_list:
-                            agregado.append(item)
-                            hash_list.append(item_hash)
+                        if feed not in saved_items:
+                            saved_items[feed] = {'hash_list': []}
+                            if item_hash not in saved_items[feed]['hash_list']:
+                                agregado.append(item)
+                                saved_items[feed]['hash_list'].append(item_hash)
 
-    save_hash_list(hash_list)
+    dump_saved_items(saved_items)
     return agregado
 
-# with open('agg_feed.txt', 'w') as agg_feed:
-#     for item in sorted(agregado, key=lambda tup: tup['date']):
-#         agg_feed.write(format_news_item(item))
 
-try:
-    while True:
-        for item in sorted(get_news(), key=lambda tup: tup['date']):
-           print_item(item)
-           sys.stdout.flush()
-        time.sleep(30)
-except KeyboardInterrupt:
-    pass
+def main():
+    try:
+        while True:
+            for item in sorted(get_news(), key=lambda tup: tup['date']):
+                print_item(item)
+                sys.stdout.flush()
+            time.sleep(30)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
